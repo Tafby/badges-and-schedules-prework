412481421d865f61902d40003066d72bad1a8d63
Tiffany Carter <tafby88@gmail.com>
2018-10-29 18:25:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,10 +8,15 @@ def batch_badge_creator(array)
  batch_badge_creator
 end
 
-def assign_rooms(speakers)
-  
+	def assign_rooms(attendees)
+  assign_rooms =  Array.new
+  attendees.each do |name|
+    room_num = (attendees.index(name) + 1).to_s
+    assign_rooms << "Hello, #{name}! You'll be assigned to room " + room_num + "!"
+  end
+  assign_rooms
 end
-
-def printer
-  
+ def printer(attendees)
+  batch_badge_creator(attendees).each {|badge_string| puts badge_string}
+  assign_rooms(attendees).each {|room_string| puts room_string}
 end
