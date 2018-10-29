4983f5e178715ccf2571a7bf0ab1a813c2e882b6
Tiffany Carter <tafby88@gmail.com>
2018-10-29 18:18:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,8 @@ def badge_maker
 end 
 
 def batch_badge_creator(array)
-  array.each do { x puts "Hello, my name is #{x}" }
+  array.each do  x 
+  puts "Hello, my name is #{x}" 
 end
 
 def assign_rooms
