8eef8d2086e901e10f13cc0d730f3ee92da15bb0
Tiffany Carter <tafby88@gmail.com>
2018-10-29 18:25:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ badge_maker = "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(array)
- 
+ batch_badge_creator = Array.new
+ attendees.each {|name| batch_badge_creator << "Hello, my name is #{name}."}
+ batch_badge_creator
 end
 
 def assign_rooms(speakers)
